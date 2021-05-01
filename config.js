2f3ee375580563560508557cfe011dbde069fd20
Kara Kelly <ksquared9595@gmail.com>
2021-04-30 21:56:45 -0500

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '509336500197910',
+        clientSecret: '12bdb4d0d08ff9103049b4be6f8e6eee'
+    }
 }
